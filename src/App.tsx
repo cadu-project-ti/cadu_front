07fddb0ba38054997cd9929f64576b6d8bc82c17
Weida Winy <winnyweida@hotmail.com>
2024-08-13 14:55:13 -0300

    features
    Adicao do cadastro
    Padronizacao de nomenclaturas

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import Home from "./assets/pages/home/Home";
-import Categorias from "./assets/pages/Categorias/Categorias";
-import Sobre from "./assets/pages/Sobre/Sobre";
-import Doacoes from "./assets/pages/Doacoes/Doacoes";
-import Contato from "./assets/pages/Contato/Contato";
+import Categorias from "./assets/pages/categorias/Categorias";
+import Sobre from "./assets/pages/sobre/Sobre";
+import Doacoes from "./assets/pages/doacoes/Doacoes";
+import Contato from "./assets/pages/contato/Contato";
 import Navbar from "./assets/components/navbar/Navbar";
 import Footer from "./assets/components/footer/Footer";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./assets/pages/login/Login";
 import { AuthProvider } from "./contexts/AuthContext";
+import Cadastro from "./assets/pages/cadastro/Cadastro";
 
 function App() {
   return (
@@ -21,10 +22,11 @@ function App() {
           
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login/>} />
-          <Route path="/Categorias" element={<Categorias />} />
-          <Route path="/Sobre" element={<Sobre />} />
-          <Route path="/Doacoes" element={<Doacoes />} />
-          <Route path="/Contato" element={<Contato />} />
+          <Route path="/cadastro" element={<Cadastro />} />
+          <Route path="/categorias" element={<Categorias />} />
+          <Route path="/sobre" element={<Sobre />} />
+          <Route path="/doacoes" element={<Doacoes />} />
+          <Route path="/contato" element={<Contato />} />
           
         </Routes>
         </ div>
