34d8cfacf248118a0efc5070e9844dbb0f9c5e0d
Joao Pedro <joaopedro.jusus@gmail.com>
2024-08-19 16:43:40 -0300

    Inserindo pasta das categorias na branch de produtos

---
+++ src/components/categorias/cardCategoria/CardCategorias.tsx
@@ -0,0 +1,14 @@
+import Categorias from '../../../models/Categorias'
+
+interface CardCategoriasProps {
+  categoria: Categorias
+}
+
+function CardCategorias({categoria}: CardCategoriasProps) {
+  return (
+    <div>
+    </div>
+  )
+}
+
+export default CardCategorias
