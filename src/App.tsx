4c4d52ff89bb1cf39db2373ce96edc20bf763d86
Joao Pedro <joaopedro.jusus@gmail.com>
2024-08-22 10:22:32 -0300

    Ajuste do merge na main

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,8 @@ import Contato from "./pages/contato/Contato";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-import Login from "./assets/pages/login/Login";
 import { AuthProvider } from "./contexts/AuthContext";
->>>>>>> login
-=======
 import Login from "./pages/login/Login";
-import { AuthProvider } from "./contexts/AuthContext";
 import Cadastro from "./pages/cadastro/Cadastro";
 import Carrinho from "./pages/carrinho/Carrinho";
 import FormularioProduto from "./components/produtos/formularioProduto/FormularioProduto";
@@ -25,38 +18,11 @@ import { ToastContainer } from "react-toastify";
 import DeletarProduto from "./components/produtos/deletarProduto/DeletarProduto";
 import ListaProdutosEdit from "./components/produtos/listaProdutos/ListaProdutosEdit";
 import FormularioCategoria from "./components/categorias/formularioCategoria/FormularioCategoria";
->>>>>>> crudProdutos
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-    <AuthProvider>
->>>>>>> login
-      <BrowserRouter>
-      <Navbar />
-      <div className='min-h-[80vh]'>
-        <Routes>
-          
-          <Route path="/" element={<Home />} />
-<<<<<<< HEAD
-=======
-          <Route path="/login" element={<Login/>} />
->>>>>>> login
-          <Route path="/Categorias" element={<Categorias />} />
-          <Route path="/Sobre" element={<Sobre />} />
-          <Route path="/Doacoes" element={<Doacoes />} />
-          <Route path="/Contato" element={<Contato />} />
-          
-        </Routes>
-        </ div>
-        <Footer />
-      </BrowserRouter>
-<<<<<<< HEAD
-=======
-=======
+
       <AuthProvider>
         <CarrinhoProvider>
           <ToastContainer/>
@@ -84,9 +50,7 @@ function App() {
             <Footer />
           </BrowserRouter>
         </CarrinhoProvider>
->>>>>>> crudProdutos
       </AuthProvider>
->>>>>>> login
     </>
   );
 }
