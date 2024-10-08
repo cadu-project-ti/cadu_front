d628e6343e52ba01be0985528e93a1bce81a4d5e
Joao Pedro <joaopedro.jusus@gmail.com>
2024-10-07 21:39:42 -0300

    Ajustando responsividade do footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,9 +5,9 @@ function Footer() {
   const data = new Date().getFullYear();
 
   return (
-    <div className="flex flex-col py-4 text-center items-center jus color-footer text-[#53c38d] border-t-emerald-400 border-t-2 font-mono">
-      <ul className="flex space-x-3">
-        <li> © {data} Team Cadu, Inc</li>
+    <div className="flex flex-col md:flex-row py-4 text-center items-center justify-between color-footer text-[#53c38d] border-t-emerald-400 border-t-2 font-mono">
+      <ul className="flex flex-col md:flex-row items-center space-y-2 md:space-y-0 md:space-x-3">
+        <li>© {data} Team Cadu, Inc</li>
         <li>| Projeto Integrador |</li>
         <Link to="/contato">
           <li className="hover:text-white">Entre em contato conosco</li>
