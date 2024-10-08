fc95ffc4cee31f3631d7809a866d3774b82a41d1
Joao Pedro <joaopedro.jusus@gmail.com>
2024-10-07 21:45:01 -0300

    Ajustando a centralização do footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,13 +5,10 @@ function Footer() {
   const data = new Date().getFullYear();
 
   return (
-    <div className="flex flex-col md:flex-row py-4 text-center items-center justify-between color-footer text-[#53c38d] border-t-emerald-400 border-t-2 font-mono">
+    <div className="flex flex-col md:flex-row py-4 text-center items-center justify-center md:justify-center color-footer text-[#53c38d] border-t-emerald-400 border-t-2 font-mono">
       <ul className="flex flex-col md:flex-row items-center space-y-2 md:space-y-0 md:space-x-3">
         <li>© {data} Team Cadu, Inc</li>
         <li>| Projeto Integrador |</li>
-        <Link to="/contato">
-          <li className="hover:text-white">Entre em contato conosco</li>
-        </Link>
       </ul>
     </div>
   );
